c8d4d3224d8fabe77c86b9f12c38c2bb94b62ebb
SaifBataineh <76657049+saefbataineh@users.noreply.github.com>
2023-12-26 08:56:44 +0300

    6-useReducer
    6-useReducer

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,84 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer, useEffect } from "react"
+import Modal from "./Modal"
+import { data } from "../../../data"
+import { v4 as uuidv4 } from "uuid"
+
 // reducer function
-
+const reducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const newPeople = [...state.people, action.payload]
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "Item Added",
+    }
+  }
+  if (action.type === "NO_VALUE") {
+    return {
+      ...state,
+      isModalOpen: true,
+      modalContent: "Please Enter value",
+    }
+  }
+  throw new Error("no matching action type")
+}
 const Index = () => {
-  return <h2>useReducer</h2>;
-};
+  const defaultState = {
+    people: [],
+    isModalOpen: false,
+    modalContent: " ",
+  }
+  const [name, setName] = useState("")
 
-export default Index;
+  // const [people, setPeople] = useState(data)
+  // const [showModal, setShowModal] = useState(false)
+
+  const [state, dispatch] = useReducer(reducer, defaultState)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (name) {
+      const newItem = { id: uuidv4(), name }
+      dispatch({ type: "ADD_ITEM", payload: newItem })
+      setName("")
+    } else {
+      dispatch({ type: "NO_VALUE" })
+    }
+  }
+
+  return (
+    <>
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      <form onSubmit={handleSubmit} className="form">
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Add Item
+        </button>
+      </form>
+      {state.people.map((person) => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "nowrap",
+              justifyContent: "center",
+              alignItems: "baseline",
+            }}
+            key={uuidv4()}
+          >
+            <h4 style={{ marginRight: 15 }}>{person.name}</h4>
+            <span className="btn"> Remove</span>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default Index
