eaedb4aeda792904209a45cf06fc6e8c7aabfabf
SaifBataineh <76657049+saefbataineh@users.noreply.github.com>
2023-12-28 10:09:49 +0300

    10-prop-types
    10-prop-types

--- src/data.js
+++ src/data.js
@@ -3,4 +3,29 @@ export const data = [
   { id: 2, name: "peter" },
   { id: 3, name: "susan" },
   { id: 4, name: "anna" },
+  {
+    id: "recmg2a1ctaEJNZhu",
+    name: "utopia sofa",
+    image: { url: "https://www.course-api.com/images/store/product-1.jpeg" },
+    price: 39.95,
+  },
+  {
+    id: "recvKMNR3YFw0bEt3",
+    name: "entertainment center",
+    image: { url: "https://www.course-api.com/images/store/product-2.jpeg" },
+    price: 29.98,
+  },
+  {
+    id: "recxaXFy5IW539sgM",
+    name: "albany sectional",
+    image: { url: "https://www.course-api.com/images/store/product-3.jpeg" },
+    price: 10.99,
+  },
+  {
+    id: "recyqtRglGNGtO4Q5",
+    name: "leather sofa",
+    image: { url: "https://www.course-api.com/images/store/product-4.jpeg" },
+    price: 9.99,
+  },
+  { id: "recNWGyP7kjFhSqw3", name: "sofa set" },
 ]
--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react"
+import { PropTypes } from "prop-types"
+import defaultImage from "../../../assets/default-image.jpeg"
 
-const Product = () => {
-  return <article className='product'>single product</article>;
-};
+const Product = ({ name, image, price }) => {
+  const url = image && image.url
+  return (
+    <>
+      <article className="product" style={{ padding: 10, borderRadius: 10 }}>
+        <h5>{name}</h5>
+        <img src={url || defaultImage} alt={name}></img>
+        <p>${price || 3.99}</p>
+      </article>
+    </>
+  )
+}
 
-export default Product;
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+}
+Product.defaultProps = {
+  image: defaultImage,
+  name: "default name",
+  price: 3.99,
+}
+export default Product
